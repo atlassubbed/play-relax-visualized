502bc34ccef6055f93aac759032a61024bfbb221
atlassubbed <atlassubbed@gmail.com>
2019-04-03 03:10:46 -0400

    temporarily fixes issue with codesandbox not respecting the index.html

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ import { AnimationQueue, GraphRenderer } from "./canvas";
 import App from "./app";
 import "./styles.css"
 
+// code sandbox isn't respecting the index.html, so let's just do this for now
+// until we figure out how to get it to use our index.html
+document.body.innerHTML = `
+  <div id="container">
+    <div id="app"></div>
+    <canvas id="canvas" width="650px" height="650px"/>
+  </div>
+`
+
 const canvas = document.getElementById("canvas");
 const appRoot = document.getElementById("app");
 const canvasRendererPlugin = new GraphRenderer(canvas);
